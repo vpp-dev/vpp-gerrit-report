60295791925e36c36ee065a59686dc51386f438c
Dave Wallace <dwallacelf@gmail.com>
2022-08-17 00:12:21 -0400

    Fix gerrit json parsing and ReStructuredText output.
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- get_changes.py
+++ get_changes.py
@@ -5,36 +5,42 @@ import json
 import argparse
 import requests
 
+
 def getjson(url, request):
-    '''Given a URL return the received JSON'''
-    r = requests.get(url+request)
+    """Given a URL return the received JSON"""
+    r = requests.get(url + request)
     if r.status_code != 200:
-        raise IOError('Downloading from Gerrit failed')
+        raise IOError("Downloading from Gerrit failed")
     return json.loads(r.text[5:])
 
-def get_changes_from_gerrit():
-    URL = 'https://gerrit.fd.io/r'
-    request = '/changes/?q=project:vpp+status:open+-is:wip&o=LABELS&o=CURRENT_REVISION&o=CURRENT_FILES&o=DETAILED_ACCOUNTS&o=CHECK&o=SUBMITTABLE'
-    changes = getjson(URL, request)
 
-    # We can only fetch 500 entries at the time, limit this to two tries
+def get_changes_from_gerrit(branch):
+    URL = "https://gerrit.fd.io/r"
+    request = f"/changes/?q=project:vpp+status:open+branch:{branch}+-is:wip&o=LABELS&o=CURRENT_REVISION&o=CURRENT_FILES&o=DETAILED_ACCOUNTS&o=CHECK&o=SUBMITTABLE"
+    changes = getjson(URL, request)
     last = changes[-1]
+    batch_count = 500  # gerrit returns at most 500 entries at a time
     try:
-        if last['_more_changes'] == True:
-            remaining = getjson(URL, request+'&S=500')
+        while last["_more_changes"] == True:
+            remaining = getjson(URL, f"{request}&S={batch_count}")
             changes += remaining
+            last = remaining[-1]
+            batch_count += 500  # gerrit returns at most 500 entries at a time
     except:
         pass
     return changes
 
+
 def main():
-    parser = argparse.ArgumentParser(description='VPP Gerrit changes download')
-    parser.add_argument('outfile', type=argparse.FileType('w'))
+    parser = argparse.ArgumentParser(description="VPP Gerrit changes download")
+    parser.add_argument("outfile", type=argparse.FileType("w"))
+    parser.add_argument("-branch", default="master")
     args = parser.parse_args()
 
-    changes = get_changes_from_gerrit()
+    changes = get_changes_from_gerrit(args.branch)
     json.dump(changes, args.outfile)
     args.outfile.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main())
--- review.py
+++ review.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-'''Create a report of which open gerrits match which maintainers'''
+"""Create a report of which open gerrits match which maintainers"""
 
 import re
 import sys
 import json
 import argparse
 import fnmatch
-import datetime
+from datetime import datetime
 from io import StringIO
 import dateutil.parser
 import requests
@@ -20,9 +20,10 @@ import requests
 # 4. Prettier reports both for maintainers and authors
 # 5. How to deal with unmaintained files and components?
 
+
 def get_maintainers_from_git():
-    '''Download the MAINTAINERS file from the fd.io repository'''
-    maintainers = 'https://git.fd.io/vpp/plain/MAINTAINERS'
+    """Download the MAINTAINERS file from the fd.io repository"""
+    maintainers = "https://git.fd.io/vpp/plain/MAINTAINERS"
     response = requests.get(maintainers)
     if response.status_code == 200:
         return response.text.splitlines()
@@ -30,7 +31,7 @@ def get_maintainers_from_git():
 
 
 def process_maintainers(text):
-    '''Parse MAINTAINERS file'''
+    """Parse MAINTAINERS file"""
     features = {}
     feature = {}
     for line in text:
@@ -38,10 +39,10 @@ def process_maintainers(text):
         if not line:
             # Save previous feature
             if feature:
-                features[feature['I']] = feature
+                features[feature["I"]] = feature
             feature = {}
             continue
-        m = re.search('^([IFMCYE]):\\s+(.*)', line)
+        m = re.search("^([IFMCYE]):\\s+(.*)", line)
         if m:
             tag = m.group(1)
             data = m.group(2)
@@ -54,29 +55,29 @@ def process_maintainers(text):
                 feature[tag] = data
         else:
             if feature:
-                feature['description'] = line
+                feature["description"] = line
 
-    features[feature['I']] = feature
+    features[feature["I"]] = feature
 
     maintainers = {}
     for _, v in features.items():
-        if 'M' not in v:
-            print('*** missing maintainer for:', v['I'], v, file=sys.stderr)
+        if "M" not in v:
+            print("*** missing maintainer for:", v["I"], v, file=sys.stderr)
             continue
-        if isinstance(v['F'], list):
-            for fl in v['F']:
-                maintainers[fl] = v['I']
+        if isinstance(v["F"], list):
+            for fl in v["F"]:
+                maintainers[fl] = v["I"]
         else:
-            maintainers[v['F']] = v['I']
+            maintainers[v["F"]] = v["I"]
     return features, maintainers
 
 
 def get_component_from_filename(maintainers, filename, debug=False):
-    '''Return the maintainer of a given file'''
+    """Return the maintainer of a given file"""
     longest_match = 0
     m = None
     for k in maintainers:
-        if '*' in k:
+        if "*" in k:
             if fnmatch.fnmatch(filename, k):
                 if len(k) > longest_match:
                     longest_match = len(k)
@@ -91,14 +92,17 @@ def get_component_from_filename(maintainers, filename, debug=False):
 
 
 def get_is_verified(change):
-    '''Return true if patch has been verified'''
-    if 'approved' in change['labels']['Verified']:
-        return True
+    """Return true if patch has been verified"""
+    try:
+        if "approved" in change["labels"]["Verified"]:
+            return True
+    except KeyError:
+        pass
     return False
 
 
 def match_maintainer(mlist, name):
-    '''Return true if maintainer is in list'''
+    """Return true if maintainer is in list"""
     if isinstance(mlist, list):
         for m in mlist:
             if m.startswith(name):
@@ -111,30 +115,30 @@ def match_maintainer(mlist, name):
 
 
 def is_reviewed(feature, reviews):
-    '''Get reviewers'''
+    """Get reviewers"""
     for k, v in reviews.items():
         if isinstance(v, dict):
-            if 'display_name' in v:
-                name = v['display_name']
+            if "display_name" in v:
+                name = v["display_name"]
             else:
-                name = v['name']
+                name = v["name"]
 
             # Is person a reviewer of given feature?
-            if match_maintainer(feature['M'], name):
-                return True, feature['M'], k
+            if match_maintainer(feature["M"], name):
+                return True, feature["M"], k
 
-    return False, feature['M'], None
+    return False, feature["M"], None
 
 
 def process_reviews(features, reviews, components):
-    '''Find reviews'''
+    """Find reviews"""
     r = {}
     for c in components:
         reviewed, reviewers, result = is_reviewed(features[c], reviews)
         if reviewed:
-            if result in ('approved', 'liked', 'recommended'):
+            if result in ("approved", "liked", "recommended"):
                 continue
-        r[c] = {'review': result, 'by': reviewers}
+        r[c] = {"review": result, "by": reviewers}
     return r
 
 
@@ -144,12 +148,12 @@ committerstream = {}
 
 
 def get_stream(assigneetype, name):
-    '''Put different assignees on different IO streams for reporting'''
-    if assigneetype == 'author':
+    """Put different assignees on different IO streams for reporting"""
+    if assigneetype == "author":
         st = authorstream
-    elif assigneetype == 'maintainer':
+    elif assigneetype == "maintainer":
         st = maintainerstream
-    elif assigneetype == 'committer':
+    elif assigneetype == "committer":
         st = committerstream
     else:
         raise ValueError()
@@ -160,82 +164,118 @@ def get_stream(assigneetype, name):
     return st[name], False
 
 
-legend = '''
-Description:
-============
-M - mergable m - merge conflict
-V - verified v - not verified
-E - not expired e - expired
-R - reviewed r - not reviewed
-S - submittable s - not submittable
-'''
+legend = """
+Legend:
+-------
+===================== ===========================
+Status Complete       Needs To Be Addressed
+===================== ===========================
+V - verified          v - not verified
+E - not expired       e - expired
+C - comments resolved c - comments not resolved
+R - reviewed          r - not reviewed
+===================== ===========================
+"""
+
 
 def print_report(report):
-    '''Sort by author / component or committable'''
+    """Sort by author / component or committable"""
     no_authors = 0
     no_committers = 0
     no_maintainers = 0
     for r in report:
-        if r['assignee'] == 'author':
-            st, new = get_stream(r['assignee'], r['owner'])
-            st.write(f'  {r["number"]} [{r["status"]} {r["last_updated_days"]}]: {r["subject"]}\n')
+        if r["assignee"] == "author":
+            st, new = get_stream(r["assignee"], f'\n{r["owner"]}')
+            st.write(
+                f'\n  `{r["number"]} <https:////gerrit.fd.io/r/c/vpp/+/{r["number"]}>`_ '
+                f'[{r["status"]} {r["last_updated_days"]}]: {r["subject"]}\n'
+            )
             no_authors += 1
-        elif r['assignee'] == 'maintainer':
+        elif r["assignee"] == "maintainer":
             # Report patch on all involved components
-            if r['missing_reviews_from']:
-                for c in r['missing_reviews_from']:
-                    st, new = get_stream(r['assignee'], c)
+            if r["missing_reviews_from"]:
+                for c in r["missing_reviews_from"]:
+                    st, new = get_stream(r["assignee"], c)
                     if new:
-                        maintainers = r['missing_reviews_from'][c]['by']
+                        maintainers = r["missing_reviews_from"][c]["by"]
                         if isinstance(maintainers, list):
-                            maintainers = ','.join(maintainers)
-                        st.write(f'{c}: {maintainers}\n')
-                    st.write(f'  {r["number"]} [{r["status"]} {r["last_updated_days"]}]: {r["subject"]}\n')
+                            maintainers = ", ".join(maintainers)
+                        st.write(f"\n{c}: {maintainers}")
+                    st.write(
+                        f'\n  `{r["number"]} <https:////gerrit.fd.io/r/c/vpp/+/{r["number"]}>`_ '
+                        f'[{r["status"]} {r["last_updated_days"]}]: {r["subject"]}\n'
+                    )
             else:
-                st, new = get_stream(r['assignee'], "unknown maintainer")
+                st, new = get_stream(r["assignee"], "unknown maintainer")
                 if new:
-                    st.write('unknown maintainer:\n')
-                st.write(f'{r["number"]} [{r["status"]} {r["last_updated_days"]}]: {r["subject"]}\n')
+                    st.write("unknown maintainer:\n")
+                st.write(
+                    f'\n  `{r["number"]} <https:////gerrit.fd.io/r/c/vpp/+/{r["number"]}>`_ '
+                    f'[{r["status"]} {r["last_updated_days"]}]: {r["subject"]}\n'
+                )
             no_maintainers += 1
-        elif r['assignee'] == 'committer':
+        elif r["assignee"] == "committer":
             no_committers += 1
-            st, new = get_stream(r['assignee'], 'committer')
-            st.write(f'  {r["number"]} [{r["status"]} {r["last_updated_days"]}]: {r["subject"]}\n')
+            st, new = get_stream(r["assignee"], "committer")
+            st.write(
+                f'\n  `{r["number"]} <https:////gerrit.fd.io/r/c/vpp/+/{r["number"]}>`_ '
+                f'[{r["status"]} {r["last_updated_days"]}]: {r["subject"]}\n\n'
+            )
         else:
-            print('***UNKNOWN ASSIGNEE***', file=sys.stderr)
+            print("***UNKNOWN ASSIGNEE***", file=sys.stderr)
 
+    header = f"""
+======================================================================
+FD.io Gerrit Change Report generated on {datetime.now().strftime('%A %Y-%m-%d, %H:%M:%S')}
+======================================================================
+"""
+    print(header)
     print(legend)
 
-    print('COMMITTERS:')
-    print('===========')
+    print(
+        "\nCommitters:"
+        "\n-----------"
+        "\n**These gerrit changes have been approved by MAINTAINERS."
+        "\nPlease perform a final review & submit.**"
+    )
     for _, st in committerstream.items():
         print(st.getvalue())
-    print('MAINTAINERS:')
-    print('============')
+    print("\nMaintainers:\n------------\n**Please review these gerrit changes.**")
     for st in sorted(maintainerstream):
         print(maintainerstream[st].getvalue())
-    print('AUTHORS:')
-    print('========')
+    print(
+        "\nAuthors:"
+        "\n--------"
+        "\n**Please rebase and fix verification failures on these gerrit changes.**"
+    )
     for st in sorted(authorstream):
-        print(f'{st}:')
+        print(f"{st}:")
         print(authorstream[st].getvalue())
 
     print(legend)
 
-    print('Patches assigned:')
-    print('     authors:', no_authors)
-    print(' maintainers:', no_maintainers)
-    print('  committers:', no_committers)
+    statistics = f"""
+Statistics:
+-----------
+================ ===
+Patches assigned
+================ ===
+authors          {no_authors}
+maintainers      {no_maintainers}
+committers       {no_committers}
+================ ===
+"""
+    print(statistics)
 
 
 def main():
-    '''Gerrit queue reporting tool.
+    """Gerrit queue reporting tool.
 
     For each patch in the Gerrit VPP queue assign the patch to either the
     author, the maintainers or to the committers.
 
-    If a patch is not verified, or it is not mergeable or it has a negative
-    review or it is not updated for the last 30 days its assigned to an author.
+    If a patch is not verified, or it has a negative review, or it is not updated
+    for the last 30 days it is assigned to an author.
 
     For review: If a patch is missing reviews from any of the affected
     components assign the patch to the maintainers.
@@ -243,12 +283,12 @@ def main():
     For submitting: If a patch is ready to merge assign the patch to the
     committers.
 
-    '''
-    parser = argparse.ArgumentParser(description='VPP Gerrit review tool')
-    parser.add_argument('--maintainers-file', type=argparse.FileType('r'),
-                        required=True)
-    parser.add_argument('--changes-file', type=argparse.FileType('r'),
-                        required=True)
+    """
+    parser = argparse.ArgumentParser(description="VPP Gerrit review tool")
+    parser.add_argument(
+        "--maintainers-file", type=argparse.FileType("r"), required=True
+    )
+    parser.add_argument("--changes-file", type=argparse.FileType("r"), required=True)
     args = parser.parse_args()
 
     # MAINTAINERS
@@ -262,32 +302,35 @@ def main():
     report = []
     for change in c:
         s = {}
-        s['is_mergeable'] = change['mergeable']
-        s['is_submittable'] = change['submittable']
-        s['is_verified'] = get_is_verified(change)
-        s['subject'] = change['subject']
-        s['unresolved_comment_count'] = change['unresolved_comment_count']
-        s['has_review_started'] = change['has_review_started']
-        if 'display_name' in change['owner']:
-            s['owner'] = change['owner']['display_name']
+        s["is_verified"] = get_is_verified(change)
+        s["subject"] = change["subject"]
+        s["unresolved_comment_count"] = change["unresolved_comment_count"]
+        s["has_review_started"] = change["has_review_started"]
+        if "display_name" in change["owner"]:
+            s[
+                "owner"
+            ] = f'**{change["owner"]["display_name"]}** <{change["owner"]["email"]}>'
         else:
-            s['owner'] = change['owner']['name']
-        s['number'] = change['_number']
+            s["owner"] = f'**{change["owner"]["name"]}** <{change["owner"]["email"]}>'
+        s["number"] = change["_number"]
 
-        reviews = change['labels']['Code-Review']
-        last_updated = dateutil.parser.parse(change['updated'])
-        s['last_updated_days'] = (datetime.datetime.now() - last_updated).days
+        try:
+            reviews = change["labels"]["Code-Review"]
+        except KeyError:
+            reviews = {}
+        last_updated = dateutil.parser.parse(change["updated"])
+        s["last_updated_days"] = (datetime.now() - last_updated).days
 
         # Find maintainer
-        rev = next(iter(change['revisions']))
-        files = change['revisions'][rev]['files']
+        rev = next(iter(change["revisions"]))
+        files = change["revisions"][rev]["files"]
         components = {}
 
         for f in files:
             component = get_component_from_filename(maintainers, f)
-            #print('COMPONENT', component, f)
+            # print('COMPONENT', component, f)
             if not component:
-                print(f'*** maintainer not found for: {f}', file=sys.stderr)
+                print(f"*** maintainer not found for: {f}", file=sys.stderr)
             else:
                 if component not in components:
                     components[component] = 1
@@ -295,29 +338,28 @@ def main():
                     components[component] += 1
 
         # Find missing reviews
-        s['missing_reviews_from'] = process_reviews(features, reviews, components)
+        s["missing_reviews_from"] = process_reviews(features, reviews, components)
 
         # Find assignee
-        status = ''
-        assignee = 'author'
-        status += 'M' if s['is_mergeable'] else 'm'
-        status += 'V' if s['is_verified'] else 'v'
-        status += 'E' if s['last_updated_days'] <= 30 else 'e'
+        status = ""
+        assignee = "author"
+        status += "V" if s["is_verified"] else "v"
+        status += "E" if s["last_updated_days"] <= 30 else "e"
+        status += "C" if s["unresolved_comment_count"] == 0 else "c"
 
         if status.isupper():  # Author has done all required
-            assignee = 'maintainer'
-            status += 'r' if s['missing_reviews_from'] else 'R'
-            status += 'S' if s['is_submittable'] else 's'
+            assignee = "maintainer"
+            status += "r" if s["missing_reviews_from"] else "R"
 
-            if not s['missing_reviews_from']:
-                assignee = 'committer'
+            if not s["missing_reviews_from"]:
+                assignee = "committer"
 
-        s['assignee'] = assignee
-        s['status'] = status
+        s["assignee"] = assignee
+        s["status"] = status
         report.append(s)
 
     print_report(report)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
